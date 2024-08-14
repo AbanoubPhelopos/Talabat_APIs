f5a6a7d7d86d86dfe283ddcf5674c5e940241702
AbanoubSaweris <abanoubphelopos12@gmail.com>
2024-08-14 14:54:47 +0200

    asking CLR to update the Db Every Run if there exist any new migration

--- Talabat.Services/obj/Debug/net8.0/Talabat.Services.AssemblyInfo.cs
+++ Talabat.Services/obj/Debug/net8.0/Talabat.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Talabat.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3334862d4e59dd68c719485f5ef6ef32c9c66b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d5334ba12089ac521f4160254bf0a2cba177e6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Talabat.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Talabat.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
